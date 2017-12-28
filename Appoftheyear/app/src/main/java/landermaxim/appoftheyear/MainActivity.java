4cfa98b9170663a020240ba1122ab62ad78913f5
Unknown <lander.vanlandeghem@student.ap.be>
2017-12-28 23:45:13 +0100

    update

--- Appoftheyear/app/src/main/java/landermaxim/appoftheyear/MainActivity.java
+++ Appoftheyear/app/src/main/java/landermaxim/appoftheyear/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
         databasemedicatie.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                //medilist.clear();
+                medilist.clear();
                 for (DataSnapshot medicatiesnapshot: dataSnapshot.getChildren()){
                     Medicate medicatie = medicatiesnapshot.getValue(Medicate.class);
                     medilist.add(medicatie);
